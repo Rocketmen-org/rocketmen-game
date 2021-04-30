b0a0db34e7985c3b3d8c60fc2057e27f282260f5
Zachary Broome <Turtle@245.223.21.198.tigernet.wifi.dyn.clemson.edu>
2021-04-30 08:42:14 -0400

    added enemy with random movement

---
+++ include/RockyManager.h
@@ -0,0 +1,42 @@
+#ifndef RockyManager_h
+#define RockyManager_h
+
+#include <vector>
+
+#include "game.h"
+#include "Sprite.h"
+#include "Rocky.h"
+
+#define NUM_ROCKYS 1
+#define ROCKY_START_X 4 * 64
+#define ROCKY_START_Y 4 * 64
+
+class RockyManager{
+	
+public:
+  RockyManager(SDL_Renderer* ren);
+  ~RockyManager();
+
+  void init(const char* image);
+  void update();
+  void render(int x, int y);
+  void quit();
+
+  bool turnOver();
+  bool defend();
+  void setAttack(int x, int y);
+  void setDefend();
+
+private:
+  
+  SDL_Renderer* obj_renderer;
+
+  vector<Rocky*> rockys;
+  vector<struct coordinate> dest;
+
+  bool attack;
+  bool turn_over;
+};
+
+
+#endif
